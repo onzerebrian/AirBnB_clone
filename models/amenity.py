29e90ad5d4ebd6684b671c10095a2facec207044
onzerebrian <onzerebrian@gmail.com>
2023-08-13 11:53:36 +0000

    tasks

--- models/amenity.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" This module contains Amenity class """
-from models.base_model import BaseModel
-
-
-class Amenity(BaseModel):
-    """ Class Amenity """
-    name = ""
--- models/state.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-""" This module contains the State class """
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """ class State """
-    name = ""
